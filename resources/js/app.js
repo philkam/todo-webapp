d38661e471f194b09447852e548dcf89f637f12d
Philip Marfo <philcho98@gmail.com>
2022-01-04 10:38:31 +0000

    Connected vue to blade

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,4 @@
+import App from './components/App'
 /**
  * First we will load all of this project's JavaScript dependencies which
  * includes Vue and other libraries. It is a great starting point when
@@ -19,7 +20,6 @@ window.Vue = require('vue').default;
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -29,4 +29,7 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
 
 const app = new Vue({
     el: '#app',
+    components:{
+        App
+    }
 });
