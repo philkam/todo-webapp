ca1090cde01d6ebc52863544c3a43750bbe2201d
Philip Marfo <philcho98@gmail.com>
2022-01-04 11:35:48 +0000

    Todos route has been setup for retrieving todos and serving it in json format

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Todo;
 
 class TodoController extends Controller
 {
@@ -13,7 +14,11 @@ class TodoController extends Controller
      */
     public function index()
     {
-        //
+        $todos = Todo::all();
+
+        return response() -> json([
+            'todos' => $todos
+        ]);
     }
 
     /**
