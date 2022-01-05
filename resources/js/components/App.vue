9407a46df363b9625b7749f32c5f9f6ef1724f69
Philip Marfo <philcho98@gmail.com>
2022-01-05 22:56:22 +0000

    Add  addItemForm,             listView to components

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,12 +1,23 @@
 <template>
 <div>
     <h1 style="margin-top: 200px; text-alignment:center"></h1>
+    <div>
+        <add-Ite
+    </div>
 </div>
 
 </template>
 
 <script>
+import addItemForm from './addItemForm';
+import listView from './listView';
+
     export default {
+        components: {
+            addItemForm,
+            listView
+        }
+      
       
     }
 </script>
